eee173d4b02aed47d6f68ddb3025bf82441fde9b
Muhammad Jaseel C K <muhammadjaseelck@gmail.com>
2024-11-11 15:05:38 +0530

    "first commit"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { marked } from 'marked';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [editorContent, setEditorContent] = useState('');
+
+  // Default markdown content
+  const defaultMarkdown = `
+  # Heading 1
+  ## Sub-heading 2
+  [Link to Google](https://www.google.com)
+  \`Inline code\`
+  \`\`\`javascript
+  const hello = "Hello World!";
+  console.log(hello);
+  \`\`\`
+  - List item 1
+  - List item 2
+  > Blockquote
+  ![Image](https://via.placeholder.com/150)
+  **Bold Text**
+  Line 1 with a carriage return.
+  Line 2 after a carriage return.
+  `;
+
+  useEffect(() => {
+    setEditorContent(defaultMarkdown);  // Set the default markdown content
+  }, []);
+
+  // Handle the input change in the editor
+  const handleEditorChange = (e) => {
+    setEditorContent(e.target.value);
+  };
+
+  // Configure marked to interpret carriage returns as <br> elements
+  const getPreviewMarkup = (markdown) => {
+    return marked(markdown, { breaks: true });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <textarea
+            id="editor"
+            value={editorContent}
+            onChange={handleEditorChange}
+            className="form-control"
+            rows="20"
+          />
+        </div>
+        <div className="col-6">
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{
+              __html: getPreviewMarkup(editorContent),
+            }}
+            className="preview"
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
